d1b8952b10cda30c80db5908c90be523a4132fb3
Devasish Mishra <devasishmishra74@gmail.com>
2026-09-21 18:06:26 +0530

    push

---
+++ level2/P17_AlphabetTriangle.java
@@ -0,0 +1,14 @@
+package level2;
+
+public class P17_AlphabetTriangle {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 0; j <= i; j++){
+                System.out.print((char) ('A' + j));
+            }
+            System.out.println();
+        }
+    }
+}
