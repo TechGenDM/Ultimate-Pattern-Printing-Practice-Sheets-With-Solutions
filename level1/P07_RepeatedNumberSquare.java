3aba81b4cc191772b049e3c0cab207a34ea15e41
Devasish Mishra <devasishmishra74@gmail.com>
2026-09-21 17:12:40 +0530

    push

---
+++ level1/P07_RepeatedNumberSquare.java
@@ -0,0 +1,13 @@
+package level1;
+
+public class P07_RepeatedNumberSquare {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
