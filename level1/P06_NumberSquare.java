1204ff674947b486a6b7dff8960f234b61a4e541
Devasish Mishra <devasishmishra74@gmail.com>
2026-09-21 17:09:21 +0530

    push

---
+++ level1/P06_NumberSquare.java
@@ -0,0 +1,13 @@
+package level1;
+
+public class P06_NumberSquare {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= 5; i++){
+            for (int j = 1; j <= 5; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
