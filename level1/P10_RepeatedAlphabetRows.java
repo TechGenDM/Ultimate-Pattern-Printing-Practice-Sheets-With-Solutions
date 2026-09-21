76ac39d900e1aff1216d7b582c5c3864455d216b
Devasish Mishra <devasishmishra74@gmail.com>
2026-09-21 17:23:42 +0530

    push

--- level1/P10_RepeatedAlphabetRows.java
+++ level1/P10_RepeatedAlphabetRows.java
@@ -1,11 +1,12 @@
 package level1;
 
-public class P08_AlphabetSquare {
+public class P10_RepeatedAlphabetRows {
     public static void main(String[] args) {
         int n = 5;
+
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
-                System.out.print((char) ('A' + j));
+                System.out.print((char) ('A' + i));
             }
             System.out.println();
         }
