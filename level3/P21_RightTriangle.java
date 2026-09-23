6c6a2252ff9dea19d5de0e6e863b683358d98e0e
Devasish Mishra <devasishmishra74@gmail.com>
2026-09-23 10:18:52 +0530

    push

---
+++ level3/P21_RightTriangle.java
@@ -0,0 +1,14 @@
+package level3;
+
+public class P21_RightTriangle {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
