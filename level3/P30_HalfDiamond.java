cddbbc9000bb6339e5f1d7c9dba45b5808f9a991
Devasish Mishra <devasishmishra74@gmail.com>
2026-09-23 10:52:22 +0530

    push

---
+++ level3/P30_HalfDiamond.java
@@ -0,0 +1,21 @@
+package level3;
+
+public class P30_HalfDiamond {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
